f20863075826fa0da831fe19fdcdd51e2d5e8ddb
Apiquet <anthony.piquet2@gmail.com>
2018-12-06 11:32:13 +0100

    Adding function to update js script

--- animated_maps/world_map/script_origin.js
+++ animated_maps/world_map/script_origin.js
@@ -1,8 +1,4 @@
 // initialize the map on the "map" div with a given center and zoom
-var map = new L.Map('map', {
-  zoom: 3
-});
-
 // create a new tile layer
 var tileUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 layer = new L.TileLayer(tileUrl,
@@ -11,9 +7,6 @@ layer = new L.TileLayer(tileUrl,
     maxZoom: 15
 });
 
-// add the layer to the map
-map.addLayer(layer);
 
-var mapfit = [[-50, 70], [-50, 80],[50, -90], [80, 80], [-50, 130]];
-map.fitBounds(mapfit);
+
 
