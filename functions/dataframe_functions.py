5c5935a4dc69748cd26e1a0b66262a31c5d9c20a
Apiquet <anthony.piquet2@gmail.com>
2018-12-11 02:13:28 +0100

    Adding functions for dataframe and folium US map

--- functions/dataframe_functions.py
+++ functions/dataframe_functions.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import numpy as np
+np.random.seed(10)
+
 def magnify():
     return [dict(selector="th",
                  props=[("font-size", "8pt")]),
@@ -20,3 +24,28 @@ def highlight_text(data, color='red', text = ''):
         is_max = data.str.contains(text)
         return pd.DataFrame(np.where(is_max, attr, ''),
                             index=data.index, columns=data.columns)
+
+def remove_randomly(df,n):
+    drop_indices = np.random.choice(df.index, n, replace=False)
+    df_subset = df.drop(drop_indices)
+    return df_subset
+
+def adding_count_of_repeated_values(df):
+    for col in df:
+        df[col] = df[col].astype(str)
+    df['LatLong'] = df['ActionGeo_Lat'] + ',' + df['ActionGeo_Long']
+    df['count'] = 0
+    List = []
+    count = []
+    index = 0
+    for value in df['LatLong']:
+        if value in List:
+            idx = List.index(value)
+            count[idx] = count[idx] + 1
+            df.iloc[index, df.columns.get_loc('count')] = count[idx]
+        else:        
+            List.append(value)
+            count.append(1)        
+            df.iloc[index, df.columns.get_loc('count')] = 1
+        index = index + 1
+    return df
